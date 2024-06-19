86149bbbafac6d7136aaf1a225da91f4a4f2f061
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-06-18 21:58:19 -0400

    added shared files for tasks and users

---
+++ src/shared/TASKS.js
@@ -0,0 +1,28 @@
+export const TASKS = [
+    {
+        id: 0,
+        name: "Wash the dishes",
+        frequency: "daily",
+        time_rating: 1,
+        assingment: null,
+        unpopular: false
+    }, 
+
+    {
+        id: 2,
+        name: "cut the grass",
+        frequency: "weekly",
+        time_rating: 3,
+        assingment: null,
+        unpopular: false
+    },
+
+    {
+        id: 3,
+        name: "clean the bathrooms",
+        frequency: "bi-weekly",
+        time_rating: 1,
+        assingment: null,
+        unpopular: false
+    }
+];
