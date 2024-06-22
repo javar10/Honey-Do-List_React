94f620df3321d597c08cf188c891dff10df92c48
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-06-22 18:26:17 -0400

    cleaned up UserList and AssignTaskBtn

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,34 +1,17 @@
 import { USERS } from '../shared/USERS';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-function UserList({setDisplayAssignList,taskList, setTaskList, assignedList, setAssignedList, usersArray, task}) {
-    // const [formData, setFormData] = useState();
+function UserList({taskList, setTaskList, assignedList, setAssignedList, usersArray, task}) {
 
     function handleClick(e) {
         setAssignedList([...assignedList, task]);
-        const taskIdx = taskList.indexOf(task);
-        console.log(taskList);
-        console.log(taskIdx);
         setTaskList(taskList.filter(li => li !== task));
-        setDisplayAssignList(false);
     }
 
     return (
-        // <select onChange={handleChange}>
-        //     {USERS.map(user => <option value={user}>{user}</option>)}
-        // </select>
-
-        // USERS.map(user => {
-        //     <Dropdown.Item >
-        //         {user}
-        //     </Dropdown.Item>
-        // })
-
         <Dropdown.Menu>
             {USERS.map(user => <Dropdown.Item onClick={handleClick}>{user}</Dropdown.Item>)}
         </Dropdown.Menu>
-
-
     );
 }
 
