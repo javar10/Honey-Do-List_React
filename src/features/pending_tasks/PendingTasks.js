248a0d97760a271c0423a801da7843d6056586c3
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-06-27 21:17:10 -0400

    updated UI

--- src/features/pending_tasks/PendingTasks.js
+++ src/features/pending_tasks/PendingTasks.js
@@ -10,10 +10,13 @@ function PendingTasks({taskList, setTaskList}) {
                     console.log(task.assignment);
                     if (task.assignment === 'pending') {
                         return (
-                            <>
-                                <li key={task.id} className="list-group-item">{task.name}</li>
-                                <AssignTaskBtn taskList={taskList} setTaskList={setTaskList} task={task}/>
-                            </>
+                            <div className="pending-tasks-div">
+                                <li key={task.id} className="list-group-item">
+                                    <h4 className="task-name">{task.name}</h4>
+                                    <p>{task.frequency}</p>
+                                    </li>
+                                <AssignTaskBtn className="assign-task-btn" taskList={taskList} setTaskList={setTaskList} task={task}/>
+                            </div>
                         )
                     } else {
                         return <></>
