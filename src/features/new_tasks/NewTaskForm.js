eea42426248890bf580c42eb33c9b9f6a1f65e6c
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-02 21:50:49 -0400

    display due date field when one-time is selected

--- src/features/new_tasks/NewTaskForm.js
+++ src/features/new_tasks/NewTaskForm.js
@@ -7,6 +7,12 @@ import { validateNewTaskForm } from './validateNewTaskForm';
 function NewTaskForm({taskList, setTaskList}) {
     const [displayTaskForm, setDisplayTaskForm] = useState(false);
 
+    const [selectedFrequency, setSelectedFrequency] = useState('');
+    const handleChange = (e, setFieldValue) => {
+        setSelectedFrequency(e.target.value);
+        setFieldValue("frequency", e.target.value);
+    }
+
     const [taskCounter, setTaskCounter] = useState(0);
     function handleSubmit(values, {resetForm}) {
         const dateCreated = new Date();
@@ -36,36 +42,51 @@ function NewTaskForm({taskList, setTaskList}) {
                         onSubmit={handleSubmit}
                         validate={validateNewTaskForm}
                     >
-                        <Form>
-                            <h2>Add New Task</h2>
-                            <FormGroup>
-                                <Label htmlFor="newTaskName" className="form-label md=2">Task Name</Label>
-                                <Field
-                                    name="task"
-                                    placeholder="Enter Task"
-                                    className="form-control"
-                                />
-                            </FormGroup>
-                            <ErrorMessage name="task">{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
+                        {({ setFieldValue }) => (
+                            <Form>
+                                <h2>Add New Task</h2>
+                                <FormGroup>
+                                    <Label htmlFor="newTaskName" className="form-label md=2">Task Name</Label>
+                                    <Field
+                                        name="task"
+                                        placeholder="Enter Task"
+                                        className="form-control"
+                                    />
+                                </FormGroup>
+                                <ErrorMessage name="task">{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
 
-                            <FormGroup>
-                                <Label htmlFor="newTaskFrequency" className="form-label md-2">Frequency   </Label>
-                                <Field 
-                                    name="frequency"
-                                    as="select"
-                                    className="form-control"
-                                >
-                                    <option disabled>--Choose an option--</option>
-                                    <option>One-Time</option>
-                                    <option>Daily</option>
-                                    <option>Weekly</option>
-                                    <option>Bi-Weekly</option>
-                                    <option>Monthly</option>
-                                </Field>
-                                <ErrorMessage name="frequency">{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
-                            </FormGroup>
-                            <Button type="submit">Submit</Button>
-                        </Form>
+                                <FormGroup>
+                                    <Label htmlFor="newTaskFrequency" className="form-label md-2">Frequency</Label>
+                                    <Field 
+                                        name="frequency"
+                                        as="select"
+                                        className="form-control"
+                                        // onChange={e => {
+                                        //     handleChange(e);
+                                        //     values.frequency = e.target.value;
+                                        // }}
+                                        onChange={e => handleChange(e, setFieldValue)}
+                                    >
+                                        <option disabled>--Choose an option--</option>
+                                        <option>One-Time</option>
+                                        <option>Daily</option>
+                                        <option>Weekly</option>
+                                        <option>Bi-Weekly</option>
+                                        <option>Monthly</option>
+                                    </Field>
+                                    <ErrorMessage name="frequency">{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
+                                </FormGroup>
+
+                                {selectedFrequency === 'One-Time' && <FormGroup>
+                                    <Label htmlFor="one-timeDetails" className="form-label md-2">Due Date</Label>
+                                    <Field
+                                        name="one-timeDetails"
+                                        className="form-control"
+                                    />
+                                </FormGroup>}
+                                <Button type="submit">Submit</Button>
+                            </Form>
+                        )}
                     </Formik>
                 </ModalBody>
             </Modal>
